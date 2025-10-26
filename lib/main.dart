4b503c31f7bbdb555ff8057a291ee81a602946d4
Your Name <your_email@example.com>
2025-10-26 21:50:16 +0300

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,14 @@ import 'package:e_commerce/features/presentation/screens/payment/my_orders_scree
 import 'package:e_commerce/features/presentation/screens/search/SearchScreen.dart';
 import 'package:e_commerce/features/presentation/screens/wishlist/WishListScreen.dart';
 
+import 'package:device_preview/device_preview.dart';
 import 'package:e_commerce/core/theme.dart';
 import 'package:e_commerce/test.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-  // DevicePreview(enabled: !kReleaseMode, builder: (context) => MyApp()),
-  MyApp(), // Wrap your app
+  DevicePreview(enabled: !kReleaseMode, builder: (context) => const MyApp()),
 );
 
 class MyApp extends StatelessWidget {
@@ -25,12 +26,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       useInheritedMediaQuery: true,
-      // locale: DevicePreview.locale(context),
-      // builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       title: "Fashion App",
-      themeMode: ThemeMode.system,
-      theme: AppThemes.light,
+      themeMode: ThemeMode.dark,
       darkTheme: AppThemes.dark,
       home: RootScreen(),
     );
